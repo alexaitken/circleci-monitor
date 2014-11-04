0c8a1af25d8e2e22f94f3b4651c2cf182e60c704
Alex Aitken <alex@alexaitken.com>
2014-11-04 18:51:25 -0500

    Allow popup to work again.

---
+++ src/popup/circleci-monitor/chrome.cljs
@@ -0,0 +1,8 @@
+(ns circleci-monitor.chrome)
+
+(def browser-action (.-browserAction js/chrome))
+
+(def background-page (js/chrome.extension.getBackgroundPage))
+
+(defn set-badge-text [text]
+  (. browser-action (setBadgeText (js-obj "text" text))))
