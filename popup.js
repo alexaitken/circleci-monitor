6f52f0eeb06583399745b1c59c26e7d8f4746010
Alex Aitken <alex@alexaitken.com>
2017-03-13 18:39:33 -0400

    Clear out for react version.

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  chrome.extension.getBackgroundPage().CircleciMonitor.startView(document);
-});
-function cleanUpView() {
-  chrome.extension.getBackgroundPage().CircleciMonitor.cleanViews();
-  removeEventListener("unload", cleanUpView)
-}
-addEventListener("unload", cleanUpView);
